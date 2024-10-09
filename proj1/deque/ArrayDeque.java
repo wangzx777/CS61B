5064709fcddca39493fbe75652367330105af201
Wamg Zixiang <2586065942@qq.com>
2024-10-09 11:10:33 +0800

    2024109

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,20 +1,20 @@
 package deque;
 
-public class ArrayDeque {
-    int size;
-    private int[] items;
-    int first;
-    int last;
+public class ArrayDeque<T> {
+    private int size;
+    private T[] items;
+    private int first;
+    private int last;
 
-    public ArrayDeque(){
-        items = new int[8];
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
         size = 0;
         first = 6;
         last = 7;
     }
 
-    public void resize(){
-        int[] a = new int[size*2];
+    public void resize() {
+        T[] a = (T[]) new Object[size * 2];
         int oldItems = items.length;
         int i = (first + 1) % items.length;
         for (int j = 0; j < items.length; j++) {
@@ -22,63 +22,63 @@ public class ArrayDeque {
             i = (i + 1) % items.length;
         }
         items = a;
-        first = items.length-1;
+        first = items.length - 1;
         last = oldItems;
     }
-    public void addLast(int x){
-        if (size == items.length){
+    public void addLast(T x) {
+        if (size == items.length) {
             resize();
         }
         items[last] = x;
-        last = (last+1+items.length) % items.length;
-        size +=1;
+        last = (last + 1 + items.length) % items.length;
+        size += 1;
     }
 
-    public void addFirst(int x){
-        if (size == items.length){
+    public void addFirst(T x) {
+        if (size == items.length) {
             resize();
         }
         items[first] = x;
-        first = (first-1+items.length) % items.length;
-        size +=1;
+        first = (first - 1 + items.length) % items.length;
+        size += 1;
     }
 
     
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public int remove(){
-        int x = items[size-1];
-        size = size-1;
+    public T remove() {
+        T x = items[size - 1];
+        size = size - 1;
         return x;
     }
 
-    public int get(int index){
-        return items[index-first];
+    public T get(int index) {
+        return items[index - first];
     }
 
-    public void printDeque(){
-        int i = (first+1) % items.length;
+    public void printDeque() {
+        int i = (first + 1) % items.length;
         for (int j = 0; j < items.length; j++) {
-            if (items[i] == 0){
+            if (items[i] == null) {
                 break;
             }
-            System.out.print(items[i]+" ");
+            System.out.print(items[i] + " ");
             i = (i + 1) % items.length;
         }
     }
 
+    public ArrayDeque(ArrayDeque other) {
+        first = other.first;
+        last = other.last;
+        size = other.size;
+        items = (T[]) new Object[other.items.length];
+        System.arraycopy(other.items, 0, items, 0, other.items.length);
+    }
+
     public static void main(String[] args) {
-        ArrayDeque arr = new ArrayDeque();
-        arr.addFirst(2);
-        arr.addFirst(1);
-        for (int i = 3; i < 9; i++) {
-            arr.addLast(i);
-        }
-        arr.addLast(10);
-        arr.addFirst(33);
-        arr.printDeque();
+
     }
 }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -3,135 +3,125 @@ package deque;
 public class LinkedListDeque<T> {
     private ListNode sentinel;
     private int size;
-    private class ListNode{
+    private class ListNode {
         T value;
         ListNode front;
         ListNode back;
-        public ListNode(T x,ListNode a, ListNode b){
+        public ListNode(T x, ListNode a,  ListNode b) {
             this.value = x;
             this.back = b;
             this.front = a;
         }
     }
     /**创建空的结点**/
-    public LinkedListDeque(){
-        this.sentinel = new ListNode(null,null,null);
+    public LinkedListDeque() {
+        this.sentinel = new ListNode(null, null, null);
         this.sentinel.front = this.sentinel;
         this.sentinel.back = this.sentinel;
         this.size = 0;
     }
     /**创建非空结点**/
-    public LinkedListDeque(T x){
-        this.sentinel = new ListNode(null,null,null);
-        this.sentinel.front = this.sentinel.back = new ListNode(x,sentinel,sentinel);
+    public LinkedListDeque(T x) {
+        this.sentinel = new ListNode(null, null, null);
+        this.sentinel.front = new ListNode(x, sentinel, sentinel);
+        this.sentinel.back = new ListNode(x, sentinel, sentinel);
         this.size = 1;
 
     }
 
-    public void addFirst(T item){
+    public void addFirst(T item) {
         ListNode p = sentinel.back;
-        /**不能让p=newListNode，这样p的指向改变，可以操作的是p.back或是p.next两个不同的指针**/
-        sentinel.back = new ListNode(item,sentinel,sentinel.back);
+        /**不能让p=newListNode，这样p的指向改变，可以操作的 是p.back或 是p.next两个不同的指针**/
+        sentinel.back = new ListNode(item, sentinel, sentinel.back);
         p.front = sentinel.back;
         this.size += 1;
     }
 
-    public void addLast(T item){
+    public void addLast(T item) {
         ListNode p = sentinel.front;
-        p.back = new ListNode(item,p,sentinel);
+        p.back = new ListNode(item, p, sentinel);
         sentinel.front = p.back;
         size += 1;
     }
 
-    public boolean isEmpty(){
-        if (sentinel.back == null){
+    public boolean isEmpty() {
+        if (sentinel.back == null) {
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 
-    public int size(ListNode p){
-        if (p.back == sentinel){
+    public int size(ListNode p) {
+        if (p.back == sentinel) {
             return 1;
         }
-        return 1+size(p.back);
+        return 1 + size(p.back);
     }
-    public int size(){
+    public int size() {
         return size(sentinel.back);
     }
 
-    public void printDeque(ListNode p){
-        if (p == sentinel){
+    public void printDeque(ListNode p) {
+        if (p == sentinel) {
             System.out.println();
             return;
         }
         System.out.print(p.value + " ");
         printDeque(p.back);
     }
-    public void printDeque(){
+    public void printDeque() {
         printDeque(sentinel.back);
     }
 
-    public T removeFirst(){
+    public T removeFirst() {
         ListNode p = sentinel.back;
-        if (p.back == sentinel){
+        if (p.back == sentinel) {
             return null;
         }
         T item = p.value;
         sentinel.back = p.back;
         p.back.front = sentinel;
-        p.front = p.back = null;
+        p.front = null;
+        p.back = null;
         size -= 1;
         return item;
     }
 
-    public T removeLast(){
+    public T removeLast() {
         ListNode p = sentinel.front;
-        if (sentinel.back == sentinel){
+        if (sentinel.back == sentinel) {
             return null;
         }
         T item = p.value;
         sentinel.front = p.front;
         p.front.back = sentinel;
-        p.front = p.back = null;
+        p.front = null;
+        p.back = null;
         size -= 1;
         return item;
     }
 
-    public T get(int index){
+    public T get(int index) {
         ListNode p = sentinel.back;
-        if (p == null){
+        if (p == null) {
             return null;
         }
-        for (int i = 0; i < index ; i++) {
+        for (int i = 0; i < index; i++) {
             p = p.back;
         }
         return p.value;
     }
 
-    public LinkedListDeque(LinkedListDeque other){
-        sentinel = new ListNode(null,null,null);
-        sentinel.front = sentinel.back = sentinel;
-        for (int i = 0; i < other.size() ; i++) {
+    public LinkedListDeque(LinkedListDeque other) {
+        sentinel = new ListNode(null, null, null);
+        sentinel.front = sentinel;
+        sentinel.back = sentinel;
+        for (int i = 0; i < other.size(); i++) {
             addLast((T) other.get(i));
         }
     }
 
     public static void main(String[] args) {
-//        LinkedListDeque<Integer> deque = new LinkedListDeque<>(32);
-//        deque.addFirst(42);
-//        deque.addLast(43);
-//        deque.printDeque();
-//        System.out.println(deque.get(1));
-//        System.out.println(deque.size);
-//        LinkedListDeque deque_dot = new LinkedListDeque(deque);
-//        deque_dot.printDeque();
-        LinkedListDeque deque = new LinkedListDeque(32);
-        deque.addLast(42);
-        deque.addLast('a');
-        deque.addLast("hello");
-        deque.printDeque();
+
     }
 }
